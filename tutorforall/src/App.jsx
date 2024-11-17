9d7f368a5f6b1773ef046e9459e0cee0f8d95870
tutorforall <tutorforallinfo@gmail.com>
2024-11-17 17:25:04 +0530

    Update App.jsx

--- tutorforall/src/App.jsx
+++ tutorforall/src/App.jsx
@@ -9,18 +9,16 @@ import ReviewsPage from './pages/ReviewsPage';
 import ContactPage from './pages/ContactPage';
 import BlogPage from './pages/BlogPage';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <Router>
+    <Router basename="/tutors">
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/experts" element={<ExpertsPage />} />
-          <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/blog" element={<BlogPage />} />
+          {/* ... other routes ... */}
         </Routes>
       </Layout>
     </Router>
